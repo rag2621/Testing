13002e162f463a8e08e284439b640d9d2a685c62
rag2621 <149160783+rag2621@users.noreply.github.com>
2025-04-10 10:58:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -497,7 +497,7 @@ mongoose
     .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("✅ MongoDB Connected");
-        app.listen(PORT, () => {
+         https.createServer(sslOptions, app).listen(PORT, () => {
             console.log(`🚀 HTTPS Server running on port ${PORT}`);
         });
     })
